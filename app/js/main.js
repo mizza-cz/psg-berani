4e1389c5ae34862bf3588c5b08af7db526a0d387
mizza-cz <mizza.cz666@gmail.com>
2021-12-09 11:58:11 +0100

    add hrac-page

--- app/js/main.js
+++ app/js/main.js
@@ -81,4 +81,40 @@ $('.article-nav__inner').slick({
   centerMode: true,
   centerPadding: '0px',
 });
+var _gallery_loading = false;
+
+$('a[data-gallery]').click(function () {
+  if (_gallery_loading) return;
+  _gallery_loading = true;
+
+  var mode = $(this).data('gallery');
+  var url = '/inc/gallery_ajax.asp?mode=' + mode;
+
+  if (mode == 1 || mode == 2) {
+    url += '&id=' + $(this).data('gallery-id');
+  }
+
+  $.getJSON(url, { format: 'json' })
+    .done(function (e) {
+      $(this).lightGallery({
+        hash: false,
+        share: false,
+        dynamic: true,
+        dynamicEl: e,
+        download: false,
+        backdropDuration: 500,
+      });
+    })
+    .fail(function (e, textStatus, error) {
+      alert('Nastala chyba při načítání galerie. Prosím zkuste to znovu.');
+      console.error('getJSON failed, status: ' + textStatus + ', error: ' + error);
+      console.error(e);
+    })
+    .always(function () {
+      _gallery_loading = false;
+    });
+
+  return false;
+});
 })
+
